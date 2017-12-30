88e2234c1f69b583455d6f589b173a0c3fdb9a02
tpak <chris@tirpak.com>
2017-12-30 21:27:59 +0000

    cleanup model.py and add initial model

--- model.py
+++ model.py
@@ -1,19 +1,23 @@
 import csv
 import cv2
 import numpy as np
+import keras
+print(keras.__version__)
+
 
 lines = []
-with open('../data/driving_log.csv') as csvfile:
+with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
+    next(reader, None) #skip the header row 
     for line in reader:
         lines.append(line)
 
 images = []
-measuerments = []
+measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = '..data/IMG/' + filename
+    current_path = 'data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
@@ -25,12 +29,12 @@ y_train = np.array(measurements)
 from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
-model = Sequential
+model = Sequential()
 model.add(Flatten(input_shape=(160,320,3)))
-model.add(Dense)
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 
 model.save('model.h5')
