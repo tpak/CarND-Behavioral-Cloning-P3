05f7ee15d1d416da8e1c59b598d92ad29582ae17
tpak <chris@tirpak.com>
2018-01-11 16:15:52 -0700

    code cleanup

--- model.py
+++ model.py
@@ -23,7 +23,7 @@ def read_csv_file(csv_dir, header=False, correction=0.2):
             next(reader, None)
         for line in reader:
             measurement = float(line[3])
-            if ((measurement <= -0.20) or (measurement >= 0.20)):
+            if ((measurement <= -0.25) or (measurement >= 0.25)):
                 img_center = csv_dir + '/IMG/' + line[0].split('/')[-1].strip()
                 img_left = csv_dir + '/IMG/' + line[1].split('/')[-1].strip()
                 img_right = csv_dir + '/IMG/' + line[2].split('/')[-1].strip()
@@ -37,11 +37,12 @@ def read_csv_file(csv_dir, header=False, correction=0.2):
 
     return (image_list, measurements)
 
-#imagePaths, measurements = read_csv_file('./CarNDTrackData2', correction=0.2)
-#imagePaths, measurements = read_csv_file('./CarNDTrackData3', correction=0.2)
-#imagePaths, measurements = read_csv_file('./CarNDTrackData4', correction=0.2)
-imagePaths, measurements = read_csv_file('./CarNDTrackData5', correction=0.25, header=True)
-#imagePaths, measurements = read_csv_file('./data', correction=0.2, header=True)
+#imagePaths, measurements = read_csv_file('./data', correction=0.3, header=True)
+#imagePaths, measurements = read_csv_file('./CarNDTrackData2', correction=0.3)
+#imagePaths, measurements = read_csv_file('./CarNDTrackData3', correction=0.3)
+#imagePaths, measurements = read_csv_file('./CarNDTrackData4', correction=0.3)
+imagePaths, measurements = read_csv_file('./CarNDTrackData5', correction=0.3, header=True)
+
 # print(len(imagePaths))
 # print(len(measurements))
 # X_train = np.array(images)
@@ -53,33 +54,37 @@ X_train, y_train = shuffle(X_train, y_train)
 print('Train images: {} Train measurements: {}'.format(len(X_train), len(y_train)))
 print('Validation samples: {} validation measurements: {}'.format(len(X_valid), len(y_valid)))
 
-def generator(X, y, batch_size=64):
+def generator(X, y, batch_size=64, train=True):
     X, y = shuffle(X, y)
     num_samples = len(X)
     print("generator # samples: ", num_samples)
 
     while 1: # Loop forever so the generator never terminates
         for offset in range(0, num_samples, batch_size):
-            #batch_size = (batch_size/2) #since we are flipping/augmenting
             batch_samples = X[offset:offset+batch_size]
             images = []
             measurements = []
             for i in range (len(batch_samples)):
                 image = cv2.imread(X[i])
+                # suggested in forums - not yet implemeted
+                # do we really need to?
                 # randomize brightness
                 ...
                 # resize and normalize
                 ...
                 images.append(image)
                 measurements.append(y[i])
-                #augment the data by flipping images
-                images.append(cv2.flip(image, 1))
-                measurements.append(y[i] * -1.0)
+                if train:
+                    # augment the data by flipping images
+                    # but only when training not validating
+                    images.append(cv2.flip(image, 1))
+                    measurements.append(y[i] * -1.0)
 
+            # convert to float
             images = np.array(images)
             measurements = np.array(measurements)
 
-            #shaken not stirred, shuffle again
+            # shaken not stirred, shuffle again
             yield shuffle(images, measurements)
 
 from keras.models import Sequential, Model
@@ -91,12 +96,13 @@ def modelCommon():
     model = Sequential()
     # normalize the image
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-    # crop so we only see the road and not the sky and the hood
-    model.add(Cropping2D(cropping=((50,20), (0,0))))
     return model
 
 def modelnVidia(model):
     # from nVidia SDC model - good baseline to start with
+    # crop so we only see the road and not the sky and the hood
+    # is this the same as nVisa model actually used? significant?
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
     model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
     model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
     model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
@@ -122,9 +128,10 @@ adam = optimizers.Adam(lr=0.0001)
 #Adamax = optimizers.Adamax(lr=0.0001)
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(generator(X_train, y_train, batch_size=64), \
+model.fit_generator(generator(X_train, y_train, batch_size=32, train=True), \
     samples_per_epoch= len(X_train), \
-    validation_data=generator(X_valid, y_valid, batch_size=64), \
-    nb_val_samples=len(X_valid), nb_epoch=5, verbose=1)
+    validation_data=generator(X_valid, y_valid, batch_size=32, train=False), \
+    nb_val_samples=len(X_valid), nb_epoch=20, verbose=1)
 
 model.save('model.h5')
+print("run complete")
