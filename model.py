75b88c2f8c61d358fc2fbac08024482ba47f2e07
tpak <chris@tirpak.com>
2018-06-03 19:44:46 +1000

    cleanup generator

--- model.py
+++ model.py
@@ -23,20 +23,19 @@ def read_csv_file(csv_dir, header=False, correction=0.0):
             next(reader, None)
         for line in reader:
             measurement = float(line[3])
-            if ((measurement <= -correction) or (measurement >= correction)):
-                img_center = csv_dir + '/IMG/' + line[0].split('/')[-1].strip()
-                img_left = csv_dir + '/IMG/' + line[1].split('/')[-1].strip()
-                img_right = csv_dir + '/IMG/' + line[2].split('/')[-1].strip()
+            img_center = csv_dir + '/IMG/' + line[0].split('/')[-1].strip()
+            img_left = csv_dir + '/IMG/' + line[1].split('/')[-1].strip()
+            img_right = csv_dir + '/IMG/' + line[2].split('/')[-1].strip()
 
-                # add images
-                image_list.append(img_center)
-                image_list.append(img_left)
-                image_list.append(img_right)
+            # add images
+            image_list.append(img_center)
+            image_list.append(img_left)
+            image_list.append(img_right)
 
-                # add steering and correction for side cams
-                measurements.append(measurement)
-                measurements.append(measurement + correction)
-                measurements.append(measurement - correction)
+            # add steering and correction for side cams
+            measurements.append(measurement)
+            measurements.append(measurement + correction)
+            measurements.append(measurement - correction)
 
     return (image_list, measurements)
 
@@ -46,13 +45,13 @@ def read_csv_file(csv_dir, header=False, correction=0.0):
 #imagePaths, measurements = read_csv_file('./CarNDTrackData4', correction=0.3)
 imagePaths, measurements = read_csv_file('./CarNDTrackData5', correction=0.2, header=True)
 
+X_train, X_valid, y_train, y_valid = train_test_split(imagePaths, measurements, test_size=0.2)
+X_train, y_train = shuffle(X_train, y_train)
+
 # print(len(imagePaths))
 # print(len(measurements))
 # X_train = np.array(images)
 # y_train = np.array(measurements)
-
-X_train, X_valid, y_train, y_valid = train_test_split(imagePaths, measurements, test_size=0.2)
-X_train, y_train = shuffle(X_train, y_train)
 
 print('Train images: {} Train measurements: {}'.format(len(X_train), len(y_train)))
 print('Validation samples: {} validation measurements: {}'.format(len(X_valid), len(y_valid)))
@@ -60,7 +59,7 @@ print('Validation samples: {} validation measurements: {}'.format(len(X_valid), 
 def generator(X, y, batch_size=32, train=True):
     X, y = shuffle(X, y)
     num_samples = len(X)
-    print("generator # samples: ", num_samples)
+    print("generator i sprocessing sample images: ", num_samples)
 
     while 1: # Loop forever so the generator never terminates
         for offset in range(0, num_samples, batch_size):
@@ -69,6 +68,7 @@ def generator(X, y, batch_size=32, train=True):
             measurements = []
             for i in range (len(batch_samples)):
                 image = cv2.imread(X[i])
+                image = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
                 # suggested in forums - not yet implemeted
                 # do we really need to?
                 # randomize brightness
@@ -88,7 +88,7 @@ def generator(X, y, batch_size=32, train=True):
             measurements = np.array(measurements)
 
             # shaken not stirred, shuffle again
-            yield shuffle(images, measurements)
+            yield sklearn.shuffle(images, measurements)
 
 from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
@@ -130,15 +130,35 @@ model = modelCommon()
 #model = simpleModel(model)
 model = modelnVidia(model)
 
-#adam = optimizers.Adam
+adam = optimizers.Adam
 #adam = optimizers.Adam(lr=0.0001)
+#adam = optimizers.Adamax()
 #Adamax = optimizers.Adamax(lr=0.0001)
 
+train_generator = generator(X_train, y_train, batch_size=32, train=True)
+validation_generator = generator(X_valid, y_valid, batch_size=32, train=False)
+
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(generator(X_train, y_train, batch_size=32, train=True), \
-    samples_per_epoch= len(X_train), \
-    validation_data=generator(X_valid, y_valid, batch_size=32, train=False), \
+history_object = model.fit_generator(train_generator, samples_per_epoch= \
+    len(X_train), validation_data=validation_generator, \
     nb_val_samples=len(X_valid), nb_epoch=3, verbose=1)
 
+# save the model
 model.save('model.h5')
 print("run complete")
+
+# print the keys contained in the history object
+print(history_object.history.keys())
+
+# shamelessly searched git for this
+# plot the training and validation loss for each epoch
+import matplotlib.pyplot as plt
+
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.savefig('loss.png', dpi=100)
+#plt.show()
